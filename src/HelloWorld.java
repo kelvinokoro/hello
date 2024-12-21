f594bbbf70ac7155b9beb8d9afb730a82a00442c
Kelvin Okoro <kelvin.okoro@ttpsc.com>
2024-12-21 10:50:34 +0100

    Here we go

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world, how are you today ?");
+
+    System.out.println("Every one wants fame");
   }
 }
